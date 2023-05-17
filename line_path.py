c27ad64ae150e719174317a64f0470903d9f3ec1
caohung20 <131183904+caohung20@users.noreply.github.com>
2023-05-17 14:40:51 +0700

    Add files via upload

--- line_path.py
+++ line_path.py
@@ -4,9 +4,46 @@ import pandas as pd
 import numpy as np
 #count branch loop(countBL)
 countBL=0
-def searchdel(arr1,arr2,numA):
-
+def searchdel(arr1,arr2,k):
     return
+"""
+loop=[1,2,3,4,5,6,8]
+def breakloop(loop,source):
+    leng=len(loop)
+    if source in loop:
+        for i in range(1,leng+1):
+            config1=[]
+            config2=[]
+            config1.append(source)
+            config2.append(source)
+            for k in range(1,i):
+                config1.append(loop[k])
+            for k in range (leng-1,i-1,-1):
+                config2.append(loop[k])
+            if len(config1)==1:
+                config1=[]
+            else:
+                print('config1:',config1) 
+            if len(config2)==1:
+                config2=[]
+            else:   
+                print('config2:',config2)
+                
+    else:
+        for i in range(0,leng):
+            config1=[]
+            config2=[]
+            for k in range(0,i):
+                config1.append(loop[k])
+            for k in range (leng-1,i-1,-1):
+                config2.append(loop[k])
+            if len(config1)!=0:
+                print('config1:',config1) 
+            if len(config2)!=0:
+                print('config2:',config2)    
+    return
+breakloop(loop,7)
+"""
 def searchList(arr1, arr2, root):
 #find the next branch of the root, it will modify 
 #the arr1 and arr2 to delete where it has searched
@@ -55,6 +92,16 @@ def searchList(arr1, arr2, root):
             k+=1
         
     return arr1,arr2,root
+def checkisland(island,my_dict):
+    for key,value in enumerate(my_dict):
+        if value in island:
+            island.remove(value)
+    if not island:
+        return
+    else:
+        print(island,'is/are island(s)')
+
+
 
 def dfs(arr1, arr2, root, check,count):
         if check == False:
@@ -62,8 +109,6 @@ def dfs(arr1, arr2, root, check,count):
             if count==0:
                 print(visited)
                 for i,value in enumerate(visited):
-                    if value in island:
-                        island.remove(value)
                     if i!=0:
                         for k in range(len(gen)):
                             if gen[k]==value:
@@ -82,8 +127,8 @@ def dfs(arr1, arr2, root, check,count):
                 dfs(arr1, arr2, root, True,count)
             else:
                 dfs(arr1, arr2, root, False,count)
-busdata=pd.read_excel("G:\do_an_tot_nghiep\Inputs33bus.xlsx",sheet_name = 1,skiprows=1)
-linedata=pd.read_excel("G:\do_an_tot_nghiep\Inputs33bus.xlsx",sheet_name = 2,skiprows=1)
+busdata=pd.read_excel("G:\do_an_tot_nghiep\input_test.xlsx",sheet_name = 0,skiprows=1)
+linedata=pd.read_excel("G:\do_an_tot_nghiep\input_test.xlsx",sheet_name = 1,skiprows=1)
 nl = linedata['FROMBUS'].values
 nr = linedata['TOBUS'].values
 flag = linedata['FLAG'].values
@@ -107,7 +152,6 @@ for i in range(len(nbus)):
         gen.append(nbus[i])
 
 island =  {n+1 for n in range(len(nbus))}
-
 
 for k,value in enumerate(gen):
     visited=[]
